20fbf0399d6f88b1f73ebfd2fe611f5a849867ab
NazgulSenpai <31775906+nazgulsenpai@users.noreply.github.com>
2022-12-21 08:58:38 -0600

    Update update.sh
    added xbps-remove -Oo and flatpak remove --unused for cleanup

--- update.sh
+++ update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo Checking xbps for updates...
-sudo xbps-install -Su
+sudo xbps-install -Suy
+sudo xbps-remove -O
+sudo xbps-remove -o
 echo Checking flatpak for updates...
 flatpak update
+flatpak remove --unused
+
