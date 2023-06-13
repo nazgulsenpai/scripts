03f1aaabba17e4bd59f96032b5ca13e3d468b77b
NazgulSenpai <31775906+nazgulsenpai@users.noreply.github.com>
2023-06-13 07:37:45 -0500

    Update update.sh
    Add -y to xbps-remove

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo Checking xbps for updates...
 sudo xbps-install -Suy
-sudo xbps-remove -O
-sudo xbps-remove -o
+sudo xbps-remove -Oy
+sudo xbps-remove -oy
 echo Checking flatpak for updates...
 flatpak update -y
 flatpak remove --unused -y
