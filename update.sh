5a0ca5363e131318dade6d301d2a2fbc75657e4c
NazgulSenpai <31775906+nazgulsenpai@users.noreply.github.com>
2023-07-13 06:23:41 -0500

    Update update.sh
    Added `vkpurge rm all` after cache cleanup

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ echo Checking xbps for updates...
 sudo xbps-install -Suy
 sudo xbps-remove -Oy
 sudo xbps-remove -oy
+vkpurge rm all 
 echo Checking flatpak for updates...
 flatpak update -y
 flatpak remove --unused -y
